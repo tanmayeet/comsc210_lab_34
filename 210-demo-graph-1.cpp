515587da1128aee8b75525b66fbb444fdd77532f
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-282.local>
2025-11-23 19:36:54 -0800

    Starting work

--- 210-demo-graph-1.cpp
+++ 210-demo-graph-1.cpp
@@ -1,3 +1,6 @@
+// COMSC210 | Lab 34 | Tanmayee Chalamalasetti
+// IDE Used: VS Code
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -5,58 +8,58 @@ using namespace std;
 const int SIZE = 7;
 
 struct Edge {
-    int src, dest, weight;
+  int src, dest, weight;
 };
 
-typedef pair<int, int> Pair;  // Creates alias 'Pair' for the pair<int,int> data type
+typedef pair<int, int>
+    Pair;  // Creates alias 'Pair' for the pair<int,int> data type
 
 class Graph {
-public:
-    // a vector of vectors of Pairs to represent an adjacency list
-    vector<vector<Pair>> adjList;
+ public:
+  // a vector of vectors of Pairs to represent an adjacency list
+  vector<vector<Pair>> adjList;
 
-    // Graph Constructor
-    Graph(vector<Edge> const &edges) {
-        // resize the vector to hold SIZE elements of type vector<Edge>
-        adjList.resize(SIZE);
+  // Graph Constructor
+  Graph(vector<Edge> const& edges) {
+    // resize the vector to hold SIZE elements of type vector<Edge>
+    adjList.resize(SIZE);
 
-        // add edges to the directed graph
-        for (auto &edge: edges) {
-            int src = edge.src;
-            int dest = edge.dest;
-            int weight = edge.weight;
+    // add edges to the directed graph
+    for (auto& edge : edges) {
+      int src = edge.src;
+      int dest = edge.dest;
+      int weight = edge.weight;
 
-            // insert at the end
-            adjList[src].push_back(make_pair(dest, weight));
-            // for an undirected graph, add an edge from dest to src also
-            adjList[dest].push_back(make_pair(src, weight));
-        }
+      // insert at the end
+      adjList[src].push_back(make_pair(dest, weight));
+      // for an undirected graph, add an edge from dest to src also
+      adjList[dest].push_back(make_pair(src, weight));
     }
+  }
 
-    // Print the graph's adjacency list
-    void printGraph() {
-        cout << "Graph's adjacency list:" << endl;
-        for (int i = 0; i < adjList.size(); i++) {
-            cout << i << " --> ";
-            for (Pair v : adjList[i])
-                cout << "(" << v.first << ", " << v.second << ") ";
-            cout << endl;
-        }
+  // Print the graph's adjacency list
+  void printGraph() {
+    cout << "Graph's adjacency list:" << endl;
+    for (int i = 0; i < adjList.size(); i++) {
+      cout << i << " --> ";
+      for (Pair v : adjList[i])
+        cout << "(" << v.first << ", " << v.second << ") ";
+      cout << endl;
     }
+  }
 };
 
 int main() {
-    // Creates a vector of graph edges/weights
-    vector<Edge> edges = {
-        // (x, y, w) —> edge from x to y having weight w
-        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{2,4,4},{2,5,5}
-    };
+  // Creates a vector of graph edges/weights
+  vector<Edge> edges = {// (x, y, w) —> edge from x to y having weight w
+                        {0, 1, 12}, {0, 2, 8}, {0, 3, 21}, {2, 3, 6}, {2, 6, 2},
+                        {5, 6, 6},  {4, 5, 9}, {2, 4, 4},  {2, 5, 5}};
 
-    // Creates graph
-    Graph graph(edges);
+  // Creates graph
+  Graph graph(edges);
 
-    // Prints adjacency list representation of graph
-    graph.printGraph();
+  // Prints adjacency list representation of graph
+  graph.printGraph();
 
-    return 0;
+  return 0;
 }
